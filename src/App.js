1de6ed67b0480e2291922cda2ada28edf9b74648
PzGlevi <pzglevitpb46@gmail.com>
2024-04-18 13:47:42 -0300

    portofolio

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import App from './App';
+
+import Header from './componentes/Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1 className="">TESTE</h1> */}
+      <Header />
+      
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <img>
+                    src=""
+                    alt=""
+                </img>
+            </div>
+        </header>
+    )
+}
