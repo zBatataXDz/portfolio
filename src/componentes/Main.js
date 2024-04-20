6a28737126aa928a44994debb07455075004b216
PzGlevi <pzglevitpb46@gmail.com>
2024-04-20 20:17:18 -0300

    Section1

--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -2,15 +2,27 @@
 export default function Main() {
     return (
         <main id="main">
-            <div id="icons">
-                <ion-icon name="logo-instagram"></ion-icon>
-                <ion-icon name="logo-discord"></ion-icon>
-                <ion-icon name="logo-github"></ion-icon>
-            </div>
+            <section id="section1">
+                <div id="meuni_inicial">
+                    <div id="icons">
+                        <div className="instagram"> 
+                            <ion-icon name="logo-instagram"></ion-icon>
+                        </div>
+                        <div className="discord">
+                            <ion-icon name="logo-discord"></ion-icon>
+                        </div>
+                        <div className="github">
+                            <ion-icon name="logo-github"></ion-icon>
+                        </div>
+                    </div>
+                    <div className="txt">
+                        <p className="titulo">👋 Olá, sou Levi Diniz</p>
+                        <p className="p1">FRONT-END</p>
+                        <p className="p2">STUDENT.</p>
+                        <p className="p3">DEVELOPER</p>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
-
-
-
-
