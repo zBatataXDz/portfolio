1266789e2f64648330bbcc98c09b5ef17d79845e
PzGlevi <pzglevitpb46@gmail.com>
2024-04-21 00:46:53 -0300

    update in main (animation)

--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -3,6 +3,11 @@ export default function Main() {
     return (
         <main id="main">
             <section id="section1">
+                <div>
+                    {/* <img className="code" src="/image/code.png"
+                        alt="codigo logico"
+                    ></img> */}
+                </div>
                 <div id="meuni_inicial">
                     <div id="icons">
                         <div className="instagram"> 
@@ -15,7 +20,29 @@ export default function Main() {
                             <ion-icon name="logo-github"></ion-icon>
                         </div>
                     </div>
-                    <div className="txt">
+                    <div id="txt">
+                        <p className="titulo">👋 Olá, sou Levi Diniz</p>
+                        <p className="p1">FULL-STACK</p>
+                        <p className="p2"> <span></span> </p>
+                        {/* <p className="p3">DEVELOPER</p> */}
+                        <p className="p3">Desenvolvedor Full-Stack com habilidades em Javascript, React, Lua e jQuery .</p>
+                    </div>
+                </div>
+            </section>
+            <section id="section2">
+                <div id="meuni_inicial">
+                    <div id="icons">
+                        <div className="instagram"> 
+                            <ion-icon name="logo-instagram"></ion-icon>
+                        </div>
+                        <div className="discord">
+                            <ion-icon name="logo-discord"></ion-icon>
+                        </div>
+                        <div className="github">
+                            <ion-icon name="logo-github"></ion-icon>
+                        </div>
+                    </div>
+                    <div id="txt">
                         <p className="titulo">👋 Olá, sou Levi Diniz</p>
                         <p className="p1">FRONT-END</p>
                         <p className="p2">STUDENT.</p>
